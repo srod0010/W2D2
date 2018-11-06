eb39c86841348b1680d9a78f719eae15770a3c6b
srod0010 <saviofrod@gmail.com>
2018-11-06 12:06:44 -0800

    Add user class to questions.rb

--- questions.rb
+++ questions.rb
@@ -13,6 +13,39 @@ end
 
 
 class User
+  attr_accessor :f_name, :l_name
+  attr_reader :id
+  
+  def self.find_by_id(id)
+    arr = QuestionsDatabase.instance.execute(<<-SQL, id)
+    SELECT
+      *
+    FROM
+      users
+    WHERE
+      id = ?;
+    SQL
+    new(arr[0])
+  end
+  
+  def self.find_by_name(fname, lname)
+    arr = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+    SELECT
+      *
+    FROM
+      users
+    WHERE
+      f_name = ? AND l_name = ?
+    SQL
+    
+    new(arr[0])
+  end
+  
+  def initialize(options)
+    @f_name = options['f_name']
+    @l_name = options['l_name']
+    @id = options['id']
+  end
   
 end
 
