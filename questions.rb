c06dfa4f0ac77a57f404bb8ce656b4c213df94dd
srod0010 <saviofrod@gmail.com>
2018-11-06 12:22:16 -0800

    Add questions and reply class to questions.rb

--- questions.rb
+++ questions.rb
@@ -50,8 +50,65 @@ class User
 end
 
 class Question
+  attr_accessor :title, :body
+  attr_reader :id, :user_id
+  
+  def self.find_by_id(id)
+    arr = QuestionsDatabase.instance.execute(<<-SQL, id)
+    SELECT
+      *
+    FROM 
+      questions
+    WHERE
+      id = ?
+    SQL
+    
+    new(arr[0])
+  end
+  
+  def self.find_by_title(title)
+    arr = QuestionsDatabase.instance.execute(<<-SQL, title)
+    SELECT
+      *
+    FROM 
+      questions
+    WHERE
+      title = ?
+    SQL
+    
+    new(arr[0])
+  end
+  
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @user_id = options['associated_author']
+  end
 end
 
-class Replies
+class Reply
+  attr_accessor :body
+  attr_reader :id, :question_id, :parent_id, :user_id
+  def self.find_by_id(id)
+    arr = QuestionsDatabase.instance.execute(<<-SQL, id)
+    SELECT
+      *
+    FROM 
+      replies
+    WHERE
+      id = ?
+    SQL
+    
+    new(arr[0])
+  end
+  
+  def initialize(options)
+    @id = options['id']
+    @question_id = options['question_id']
+    @parent_id = options['parent_id']
+    @user_id = options['user_id']
+    @body = options['body']
+  end
   
 end
