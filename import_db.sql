ffb3f77eedc5ca16f53a387d7a84e3d5e6261aa4
srod0010 <saviofrod@gmail.com>
2018-11-06 17:06:16 -0800

    Add medium queries

--- import_db.sql
+++ import_db.sql
@@ -1,11 +1,14 @@
 PRAGMA foreign_keys = ON;
-DROP TABLE IF EXISTS questions;
+
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   f_name TEXT NOT NULL,
   l_name TEXT NOT NULL
 );
+
+DROP TABLE IF EXISTS questions;
 
 CREATE TABLE questions (
   id INTEGER PRIMARY KEY,
@@ -16,6 +19,8 @@ CREATE TABLE questions (
   FOREIGN KEY (associated_author) REFERENCES users(id)
 );
 
+DROP TABLE IF EXISTS question_follows;
+
 CREATE TABLE question_follows (
   id INTEGER PRIMARY KEY,
   user_id INTEGER,
@@ -24,6 +29,8 @@ CREATE TABLE question_follows (
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (question_id) REFERENCES questions(id)
 );
+
+DROP TABLE IF EXISTS replies;
 
 CREATE TABLE replies (
   id INTEGER PRIMARY KEY,
@@ -37,13 +44,15 @@ CREATE TABLE replies (
   FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
+DROP TABLE IF EXISTS question_likes;
+
 CREATE TABLE question_likes (
   id INTEGER PRIMARY KEY,
   user_id INTEGER,
   question_id INTEGER,
   
   FOREIGN KEY(user_id) REFERENCES users(id),
-  FOREIGN KEY(question_id) REFERENCES question(id)
+  FOREIGN KEY(question_id) REFERENCES questions(id)
   
 );
 
@@ -51,35 +60,50 @@ INSERT INTO
   users (f_name, l_name)
 VALUES
   ('Savio', 'Rodrigues'),
-  ('Jerrick', 'Shaw');
+  ('Jerrick', 'Shaw'),
+  ('Jane', 'Doe'),
+  ('John', 'Smith');
   
+
 INSERT INTO
   questions (title, body, associated_author)
 VALUES
   ('How do you use SQL?', 'I''m having trouble with SQL. Can anyone please help?', 2),
-  ('How do you set up an ORM DB?', 'The video was hard to follow.', 1);
-  
+  ('How do you set up an ORM DB?', 'The video was hard to follow.', 1),
+  ('Add','How do you add to your database?', 3),
+  ('Ruby', 'How do you write Ruby?', 4);
+
 INSERT INTO
   question_follows (user_id, question_id)
 VALUES
-  (1,2),
+  (1,3),
   (1,1),
+  (2,3),
   (2,1),
+  (3,2),
+  (1,2),
+  (4,2),
   (2,2);
   
+
 INSERT INTO
   replies (question_id, user_id, parent_id, body)
 VALUES
   (2,2,NULL,'I''m having a hard time myself'),
   (1,1,NULL,'Watch the videos from W3D1.'),
   (2,1,1,'I''m glad I''m not the only one.');
-  
+
 INSERT INTO 
   question_likes (user_id, question_id)
 VALUES
+  (1,3),
+  (2,3),
+  (3,3),
+  (2,2),
   (1,1),
-  (2,2);
+  (4,2);
   
+
   
   
   
--- questions.rb
+++ questions.rb
@@ -1,5 +1,6 @@
 require 'singleton'
 require 'sqlite3'
+require 'byebug'
 
 class QuestionsDatabase < SQLite3::Database
   include Singleton
@@ -47,6 +48,17 @@ class User
     @id = options['id']
   end
   
+  def followed_questions
+    QuestionFollow.followed_questions_for_user_id(@id)
+  end
+  
+  def authored_questions  
+    Question.find_by_author_id(@id)
+  end
+  
+  def authored_replies
+    Reply.find_by_user_id(@id)
+  end
 end
 
 class Question
@@ -79,12 +91,55 @@ class Question
     new(arr[0])
   end
   
+  def self.find_by_author_id(author_id)
+    arr = QuestionsDatabase.instance.execute(<<-SQL, author_id)
+    SELECT
+      *
+    FROM 
+      questions
+    WHERE
+      associated_author = ?
+    SQL
+    
+    result = []
+    arr.each do |question|
+      result << new(question)
+    end
+    result
+  end
+  
+  def self.most_followed(n)
+    QuestionFollow.most_followed_questions(n)
+  end
+  
   def initialize(options)
     @id = options['id']
     @title = options['title']
     @body = options['body']
     @user_id = options['associated_author']
   end
+  
+  def followers
+    QuestionFollow.followers_for_question_id(@id)
+  end
+  
+  def author
+    arr = QuestionsDatabase.instance.execute(<<-SQL, @user_id)
+    SELECT
+      *
+    FROM
+      users
+    WHERE
+      id = ?
+    SQL
+    
+    User.new(arr[0])
+  end
+  
+  def replies
+    Reply.find_by_question_id(@id)
+  end
+
 end
 
 class Reply
@@ -103,6 +158,42 @@ class Reply
     new(arr[0])
   end
   
+  def self.find_by_question_id(question_id)
+    arr = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+    SELECT
+      *
+    FROM 
+      replies
+    WHERE
+      question_id = ?
+    SQL
+    
+    result = []
+    arr.each do |reply|
+      result << Reply.new(reply)
+    end
+    
+    result
+  end
+  
+  def self.find_by_user_id(user_id)
+    arr = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+    SELECT
+      *
+    FROM 
+      replies
+    WHERE
+      user_id = ?
+    SQL
+    
+    result = []
+    arr.each do |reply|
+      result << Reply.new(reply)
+    end
+    
+    result
+  end
+  
   def initialize(options)
     @id = options['id']
     @question_id = options['question_id']
@@ -111,4 +202,177 @@ class Reply
     @body = options['body']
   end
   
+  def author
+    User.find_by_id(@user_id)
+  end
+  
+  def question
+    Question.find_by_id(@question_id)
+  end
+  
+  def parent_reply
+    raise 'No parent reply' unless @parent_id
+    Reply.find_by_id(@parent_id)
+  end
+  
+  def child_replies
+    arr = QuestionsDatabase.instance.execute(<<-SQL, @id)
+    SELECT
+      *
+    FROM
+      replies
+    WHERE
+      parent_id = ?
+    SQL
+    
+    
+    result = []
+    arr.each do |reply|
+      result << Reply.new(reply)
+    end
+    result
+  end
 end
+
+class QuestionFollow
+  def self.followers_for_question_id(question_id)
+    arr = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+    SELECT
+      users.*
+    FROM
+      users
+    JOIN
+      question_follows
+    ON
+      users.id = question_follows.user_id
+    JOIN
+      questions
+    ON
+      questions.id = question_follows.question_id
+    WHERE
+      questions.id = ?
+    SQL
+    
+    result = []
+    arr.each do |user|
+      result << User.new(user)
+    end
+    result
+  end
+  
+  def self.followed_questions_for_user_id(user_id)
+    arr = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+    SELECT
+      questions.*
+    FROM 
+      questions 
+    JOIN
+      question_follows
+    ON
+      questions.id = question_follows.question_id
+    JOIN
+      users
+    ON
+      question_follows.user_id = users.id
+    WHERE
+      users.id = ?
+    SQL
+    
+    result = []
+    arr.each do |question|
+      result << Question.new(question)
+    end
+    result 
+  end
+  
+  def self.most_followed_questions(n)
+    arr = QuestionsDatabase.instance.execute(<<-SQL, n)
+    SELECT
+      questions.id, COUNT(question_follows.user_id) AS num_followers
+    FROM
+      questions
+    JOIN
+      question_follows
+    ON
+      question_follows.question_id = questions.id
+    JOIN
+      users
+    ON
+      users.id = question_follows.user_id
+    GROUP BY
+      questions.id
+    ORDER BY 
+      num_followers DESC
+    LIMIT
+      ?
+    SQL
+    
+    result = []
+    arr.each do |hash|
+      q = Question.find_by_id(hash['id'])
+      result << q
+    end
+    result
+  end
+  
+  
+  
+end
+
+class QuestionLike
+  
+  def self.likers_for_question_id(id)
+    arr = QuestionsDatabase.instance.execute(<<-SQL, id)
+    SELECT
+      users.*
+    FROM
+      users
+    JOIN
+      question_likes
+    ON
+      users.id = question_likes.user_id
+    JOIN
+      questions
+    ON
+      question_likes.question_id = questions.id
+    WHERE
+      questions.id = ?
+    SQL
+    
+    result = []
+    arr.each do |user|
+      result << User.new(user)
+    end
+    result
+  end
+  
+  def self.liked_questions_for_user_id(user_id)
+    arr = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+    SELECT
+      questions.*
+    FROM
+      questions
+    JOIN
+      question_likes
+    ON
+      questions.id = question_likes.question_id
+    JOIN
+      users
+    ON
+      question_likes.user_id = users.id
+    WHERE
+      users.id = ?
+    SQL
+    
+    result = []
+    arr.each do |question|
+      result << Question.new(question)
+    end
+    result
+  end
+end  
+
+if __FILE__ == $PROGRAM_NAME
+  reply = Reply.find_by_id(1)
+  reply.child_replies
+end 
